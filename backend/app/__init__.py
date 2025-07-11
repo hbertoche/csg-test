93c1e0142d2caa1a053fe24ca0299e45b23d9221
unknown <hj.bertoche@gmail.com>
2025-07-10 21:10:00 -0300

    Backend:
    - Fixed CORs error when fetching from the frontend
    
    Frontend:
    
    - Created example.env.local for better management of the backend url
    - Updated README.md for better instructions and better information
      regarding the project
    - Updated page.tsx to use the lib/api and the components created
    - Updated api.ts to use the backend url from the .env

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,12 @@
+
 from flask import Flask
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
-    from app.controller.routes import bp
-    app.register_blueprint(bp)
+    from .controller.routes import bp as routes_bp
+    app.register_blueprint(routes_bp)
 
     return app
--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,5 +1,7 @@
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+
 export async function fetchRepos(username: string) {
-  const res = await fetch("/api/repos", {
+  const res = await fetch(`${BACKEND_URL}/api/repos`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ username }),
